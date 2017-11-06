e6a13fcf6081054507a89d05c5fa06e0404b2b6d
himanish58 <himanish.kabiraj@gmail.com>
2017-11-06 20:25:55 +0530

    Created the validateForm function in es6.

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,22 @@
 let username = document.querySelector('#username');
 let password = document.querySelector('#password');
-let login = document.querySelector('form');
+let login = document.querySelector('#loginButton');
 let user = {
   username: 'himanish58',
   password: '1234',
 };
 
-function validateForm() {
+let validateForm = () => {
+  console.log('hello');
   if (username.value === user.username && password.value === user.password) {
+    window.location.href = '/homepage.html';
     return true;
   }
 
   alert('Please enter a valid username and password');
+  username.value = '';
+  password.value = '';
   return false;
 };
 
-// login.addEventListener('submit', validate);
+login.addEventListener('click', validateForm);
